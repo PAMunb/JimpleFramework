1a1c5ac0e219a5e45655b83e75c17907c8c5fe0b
Fausto Carvalho <faustocarva@gmail.com>
2020-09-27 16:26:25 -0300

    Bring back code that was deleted in merge conflict.

--- src/test/java/lang/jimple/internal/TestDecompiler.java
+++ src/test/java/lang/jimple/internal/TestDecompiler.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import io.usethesource.vallang.IConstructor;
@@ -14,10 +15,11 @@ import io.usethesource.vallang.impl.persistent.ValueFactory;
 
 public class TestDecompiler {
 
+
 	@Test 
 	public void decompileClassWithFields() {
 		try {
-			File classFile = new File("./target/test-classes/samples/ClassWithFields.class"); 			
+			File classFile = new File("./target/test-classes/samples/ClassWithFields.class"); 
 			assertNotNull(classFile);
 			
 			IValueFactory vf = ValueFactory.getInstance();
@@ -30,25 +32,8 @@ public class TestDecompiler {
 			e.printStackTrace();
 			fail(e.getLocalizedMessage());
 		}
-	}	
+	}
 	
-	@Test 
-	public void decompileNestedInterface() {
-		try {
-			File classFile = new File("./target/test-classes/samples/NestedInterface.class"); 			
-			assertNotNull(classFile);
-			
-			IValueFactory vf = ValueFactory.getInstance();
-			Decompiler decompiler = new Decompiler(vf);
-			IConstructor c = decompiler.decompile(new FileInputStream(classFile), null);
-			
-			assertNotNull(c);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			fail(e.getLocalizedMessage());
-		}
-	}	
 	@Test 
 	public void decompileInterface() {
 		try {
@@ -122,6 +107,59 @@ public class TestDecompiler {
 		}
 	}
 	
+	@Test 
+	public void decompileNestedInterface() {
+		try {
+			File classFile = new File("./target/test-classes/samples/NestedInterface.class"); 			
+			assertNotNull(classFile);
+			
+			IValueFactory vf = ValueFactory.getInstance();
+			Decompiler decompiler = new Decompiler(vf);
+			IConstructor c = decompiler.decompile(new FileInputStream(classFile), null);
+			
+			assertNotNull(c);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			fail(e.getLocalizedMessage());
+		}
+	}	
 	
+	@Test 
+	@Ignore
+	public void decompileStreamAPI() {
+		try {
+			File classFile = new File("./target/test-classes/samples/StreamAPI.class"); 			
+			assertNotNull(classFile);
+			
+			IValueFactory vf = ValueFactory.getInstance();
+			Decompiler decompiler = new Decompiler(vf);
+			IConstructor c = decompiler.decompile(new FileInputStream(classFile), null);
+			
+			assertNotNull(c);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			fail(e.getLocalizedMessage());
+		}
+	}	
+	
+	@Test 
+	public void decompileAndroidClass() {
+		try {
+			File classFile = new File("./target/test-classes/android-app/oms/wmessage/main.class"); 			
+			assertNotNull(classFile);
+			
+			IValueFactory vf = ValueFactory.getInstance();
+			Decompiler decompiler = new Decompiler(vf);
+			IConstructor c = decompiler.decompile(new FileInputStream(classFile), null);
+			
+			assertNotNull(c);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			fail(e.getLocalizedMessage());
+		}
+	}
 
 }
--- src/test/java/samples/NestedInterface.java
+++ src/test/java/samples/NestedInterface.java
@@ -1,18 +1,16 @@
 package samples;
 
-interface MyInterfaceA{  
-    void display();  
-}  
-      
-class NestedInterface 
-    implements MyInterfaceA{  
-     public void display(){
-         System.out.println("Nested interface method");
-     }  
-      
-     public static void main(String args[]){  
-         MyInterfaceA obj=
-                 new NestedInterface(); 
-      obj.display();  
-     }  
+interface MyInterfaceA {
+	void display();
+}
+
+class NestedInterface implements MyInterfaceA {
+	public void display() {
+		System.out.println("Nested interface method");
+	}
+
+	public static void main(String args[]) {
+		MyInterfaceA obj = new NestedInterface();
+		obj.display();
+	}
 }
