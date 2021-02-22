b780c7f3c692715bfd8296cd6f2086ced579c8ce
Fausto Carvalho <faustocarva@gmail.com>
2021-02-22 18:02:02 -0300

    adds a array test case

---
+++ src/test/java/samples/arrays/ArrayExample.java
@@ -0,0 +1,28 @@
+package samples.arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayExample {
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> nums = Arrays.asList(-3, 0, 1, 8);
+
+        Runnable r = () -> nums.forEach(n -> {
+
+            if (n < 0) System.out.println("Negative: " + n);
+
+            else System.out.println("Positive: " + n);
+
+        });
+
+        Thread t = new Thread(r);
+
+        t.start();
+
+        t.join();    
+
+    }
+
+}
+
